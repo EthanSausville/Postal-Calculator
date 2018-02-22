2b7013ef65f7e157b81c1d4b8c09f6b1ad37ed1e
EthanSausville <ethan.sausville@gmail.com>
2018-02-21 18:41:19 -0800

    Upload

---
+++ Postal Calculator/Default.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Postal_Calculator.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Postal Calculator<br />
+            <div>Width: <asp:TextBox ID="widthTextBox" runat="server" AutoPostBack="True" OnTextChanged="widthTextBox_TextChanged"></asp:TextBox></div>
+            <div> Height: <asp:TextBox ID="heightTextBox" runat="server" AutoPostBack="True" OnTextChanged="heightTextBox_TextChanged"></asp:TextBox></div>
+            <div>Length: <asp:TextBox ID="lengthTextBox" runat="server" AutoPostBack="True" OnTextChanged="lengthTextBox_TextChanged"></asp:TextBox></div>
+
+            <br />
+
+            <div><asp:RadioButton ID="groundRadioButton" runat="server" AutoPostBack="True" GroupName="shipType" Text="Ground" Checked="True" OnCheckedChanged="groundRadioButton_CheckedChanged1" /></div>
+            <div><asp:RadioButton ID="airRadioButton" runat="server" AutoPostBack="True" GroupName="shipType" Text="Air" OnCheckedChanged="airRadioButton_CheckedChanged" /></div>
+            <div><asp:RadioButton ID="nextDayRadioButton" runat="server" AutoPostBack="True" GroupName="shipType" Text="Next Day" OnCheckedChanged="nextDayRadioButton_CheckedChanged" /></div>
+
+            <br />
+
+            <asp:Label ID="resultLabel" runat="server"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Postal Calculator/Default.aspx.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Postal_Calculator
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void groundRadioButton_CheckedChanged1(object sender, EventArgs e)
+        { getCost(); }
+
+        protected void airRadioButton_CheckedChanged(object sender, EventArgs e)
+        { getCost(); }
+
+        protected void nextDayRadioButton_CheckedChanged(object sender, EventArgs e)
+        { getCost(); }
+
+        protected void widthTextBox_TextChanged(object sender, EventArgs e)
+        { getCost(); }
+
+        protected void heightTextBox_TextChanged(object sender, EventArgs e)
+        { getCost(); }
+
+        protected void lengthTextBox_TextChanged(object sender, EventArgs e)
+        { getCost(); }
+
+        private void getCost()
+        {
+            if (groundRadioButton.Checked)
+                writeString(0.15);
+            if (airRadioButton.Checked)
+                writeString(0.25);
+            if (nextDayRadioButton.Checked)
+                writeString(0.45);
+        }
+
+        private void writeString(double rate)
+        {
+            if (widthTextBox.Text != "" && heightTextBox.Text != "")
+            {
+                double cost = getPrice(rate);
+                resultLabel.Text = string.Format("Your parcel will cost {0:C} to ship.", cost);
+            }
+        }
+
+        private double getPrice(double rate)
+        {
+            double width = double.Parse(widthTextBox.Text);
+            double height = double.Parse(heightTextBox.Text);
+            double length = 1;
+            if (lengthTextBox.Text != "")
+                length = double.Parse(lengthTextBox.Text);
+            double cost = width * height * length * rate;
+            return cost;
+        }
+    }
+}
